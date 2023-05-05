cd8802667789f2109fa4d2b6f253cde45ae27d80
Eduardo Cano <canoeduardojesus@gmail.com>
2023-05-05 17:55:03 -0300

    prueba tecnica

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Project3 from './MouseFollower.jsx'
+// import Project3 from './MouseFollower.jsx'
 // import TicTacToe from './TicTacToe.jsx'
+import PruebaTecnica1 from './PruebaTecnica1'
 import './style/App.css'
 
 function App () {
   return (
     // <TicTacToe />
-    <Project3 />
+    // <Project3 />
+    <PruebaTecnica1 />
   )
 }
 
--- src/PruebaTecnica1.jsx
+++ src/PruebaTecnica1.jsx
@@ -1,6 +1,21 @@
+import { useCatImage } from './hooks/useCatImage.js'
+import { useCatFact } from './hooks/useCatFact.js'
+import './style/PruebaTecnia1.css'
 
 export default function PruebaTecnica1 () {
+  const { fact, refreshFact } = useCatFact()
+  const { imageUrl } = useCatImage({ fact })
+
+  const handleClick = async () => {
+    refreshFact()
+  }
+
   return (
-    <div>PruebaTecnica1</div>
+    <main>
+      <h1>Kitten app</h1>
+      <button onClick={handleClick}>Get new fact</button>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt='cat' />}
+    </main>
   )
 }
